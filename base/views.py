a515d14f4f0a5e69550da46a1595e8a46e04f5b2
Sandi Stokanovic <sandi.stokanovic@stu.ibu.edu.ba>
2022-05-08 13:14:16 +0200

    dynamic routing and fixed structure

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Learn python!', },
+    {'id': 2, 'name': 'Learn frontend', },
+    {'id': 3, 'name': 'Learn django', }
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
