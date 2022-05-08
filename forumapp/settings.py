a44ed23216dba9ffae99c05c27049b7ba6e8af47
Sandi Stokanovic <sandi.stokanovic@stu.ibu.edu.ba>
2022-05-08 12:09:31 +0200

    basic templating

--- forumapp/settings.py
+++ forumapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig', #connecting directly to app
+    'base.apps.BaseConfig',  # connecting directly to app
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'forumapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
